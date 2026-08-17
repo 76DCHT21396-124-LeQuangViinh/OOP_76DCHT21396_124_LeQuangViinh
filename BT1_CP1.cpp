d3a04d9a7d253cdaabe085e0929fd05f52885948
76DCHT21396-124-LeQuangViinh <317967127+76DCHT21396-124-LeQuangViinh@users.noreply.github.com>
2026-08-18 01:25:11 +0700

    Create BT1_CP1.cpp
    Signed-off-by: 76DCHT21396-124-LeQuangViinh <317967127+76DCHT21396-124-LeQuangViinh@users.noreply.github.com>

---
+++ BT1_CP1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct NhanVien{
+    string manv;
+    string tennv;
+    long luong;
+    int heso;
+};
+
+int main(){
+    NhanVien nv[100];
+    int n;
+
+    cout << "Nhap so luong nhan vien: ";
+    cin >> n;
+
+    return 0;
+}
+
